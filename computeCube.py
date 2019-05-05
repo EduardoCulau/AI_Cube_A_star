8a4b60cfae89ccabc41d55eb6d5489a6bfb9deb6
EduardoCulau <eduardo.culau@gmail.com>
2019-05-05 08:48:50 +0000

    Results compact

--- computeCube.py
+++ computeCube.py
@@ -3,11 +3,13 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 
+MIN_REPETITION = 50;
+
 CUBE_SIZE    = 20;
 OBSTRUCTIONS = 30;
-ROUTES		 = 100_000;
-THREADS		 = 8;
-FILE		 = "./bin/results.csv"
+ROUTES       = 100_000;
+THREADS      = 8;
+FILE         = "./bin/results.csv"
 
 #Combine the arguments
 #executable = "./bin/cube" + "-c " + CUBE_SIZE + "-o " + OBSTRUCTIONS + "-r " + ROUTES + "-t " + THREADS + "-f " + FILE;
@@ -17,7 +19,7 @@ FILE		 = "./bin/results.csv"
 #os.system(executable);
 
 #Pre computed file
-PRE_COMPUTED_FILE = "./bin/results_20_30_240000.csv"
+PRE_COMPUTED_FILE = "./bin/results_compact_20_30_240000.csv"
 
 #Define the data list
 data = []
@@ -26,6 +28,10 @@ data = []
 with open(PRE_COMPUTED_FILE, 'r') as csvFile:
 	readCSV = csv.reader(csvFile, delimiter=';');
 	c = 0;
+	#Get the data
 	for row in readCSV:
-		data.append((int(row[0]), float(row[1])));
-		c = c+1; print(c);
+		data.append((int(row[0]), int(row[1]), float(row[2])));
+		print(c + " = " data[-1]); c = c+1;
+
+#Cut the zeros, from back to front
+
