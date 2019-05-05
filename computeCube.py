ec16f83786dc430e584bacda5eb801fb708dfe7e
EduardoCulau <eduardo.culau@gmail.com>
2019-05-05 09:47:51 +0000

    Generate the Image

--- computeCube.py
+++ computeCube.py
@@ -7,7 +7,7 @@ MIN_REPETITION = 50;
 
 CUBE_SIZE    = 20;
 OBSTRUCTIONS = 30;
-ROUTES       = 100_000;
+ROUTES       = 240_000;
 THREADS      = 8;
 FILE         = "./bin/results.csv"
 
@@ -27,11 +27,32 @@ data = []
 #Open the csvFile
 with open(PRE_COMPUTED_FILE, 'r') as csvFile:
 	readCSV = csv.reader(csvFile, delimiter=';');
-	c = 0;
 	#Get the data
+	print("CSV Data");
 	for row in readCSV:
 		data.append((int(row[0]), int(row[1]), float(row[2])));
-		print(c + " = " data[-1]); c = c+1;
+		print(data[-1]);
 
-#Cut the zeros, from back to front
+#Cut the zeros and cost with less than the minimun repetition from back to front. Equal of data[-1:]
+for i in range(len(data)-1, -1, -1):
+        if data[i][0] == 0 or data[i][1] < MIN_REPETITION:
+                data.pop(i);
+
+#Print the filtered data and get cost and time.
+print("\n\nFiltered Data");
+cost = []; avg_time = [];
+for r in data:
+        print(r);
+        #Get cost and time from data
+        cost.append(r[0]); avg_time.append(r[2]*1000);
+
+#Plot
+plt.plot(cost, avg_time, cost, avg_time,'r.')
+plt.xlabel('Distance (manhattan)')
+plt.ylabel('Average Time (ms)')
+plt.title('Average time VS Distance\n' + "Cube=" + str(CUBE_SIZE) + " | Obst=" + str(OBSTRUCTIONS) + "% | Routes=" + str(ROUTES));
+plt.grid(True);
+plt.show();
+
+                
 
